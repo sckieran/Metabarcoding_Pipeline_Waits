e77249685cbc3c6f5b5f52c5ee78b4f79284b1aa
sckieran <53580356+sckieran@users.noreply.github.com>
2023-07-02 10:53:49 -0700

    Update step_1_make_local_database.sh

--- step_1_make_local_database.sh
+++ step_1_make_local_database.sh
@@ -2,7 +2,7 @@
 #
 
 
-while getopts ":n:t:g:d:r:h:" opt; do
+while getopts ":n:t:g:d:r:h:l:" opt; do
   case $opt in
     n) prefix="$OPTARG"
     ;;
@@ -15,6 +15,8 @@ while getopts ":n:t:g:d:r:h:" opt; do
     r) retmax="$OPTARG"
     ;;
     h) db_dirr="$OPTARG"
+    ;;
+    l) rlib="$OPTARG"
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
     exit 1
@@ -35,6 +37,6 @@ mkdir -p $db_dirr
 module load R/4.2.3
 module load ncbi-blast
 #run rentrez#
-Rscript ${dirr}/scripts/query_rentrez.R -n $prefix -t $taxlist -g $genelist -d ${dirr}/$db_dirr -r $retmax 
+Rscript ${dirr}/scripts/query_rentrez.R ${rlib} -n $prefix -t $taxlist -g $genelist -d ${dirr}/$db_dirr -r $retmax 
 
 echo "done downloading reference sequences. If you have additional off-target sequences to add here, simply ensure that they are in this folder, that each file name ends *_gene1_sequences.fasta and that each sequence line begins with a '>' and a valid NCBI accession number to continue. You must have a different copy of these fastas for each gene1...geneN that you are building a database for, the program will only include fastas for a gene's reference if it ends *_gene1_sequences.fasta."
