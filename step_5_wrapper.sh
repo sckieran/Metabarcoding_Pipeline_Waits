8db70801a4a7bbaf619dbc8062fec9ae8af499b3
sckieran <53580356+sckieran@users.noreply.github.com>
2023-07-01 23:10:28 -0700

    Add files via upload

---
+++ step_5_wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -o step_5_slurm.%j.out
+#SBATCH -e step_5_slurm.%j.err
+#SBATCH -C "ceph"
+#SBATCH -t 2880
+#SBATCH -J step_5
+
+dir=$PWD #path to the your_project directory. If you are running this code from inside that directory (recommended), you can put $PWD here.
+prefix=your_project
+genelist=$PWD/genelist #path to genelist
+cutoff=97 #your identity % cutoff. Non-inclusive. 
+
+bash step_4_filter_and_blast_local.sh -d ${dir} -n ${prefix} -g ${genelist} -c ${cutoff}
+
