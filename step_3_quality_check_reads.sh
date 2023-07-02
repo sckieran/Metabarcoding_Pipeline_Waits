65d63227793ab681fa8da220274ec9d374345996
sckieran <53580356+sckieran@users.noreply.github.com>
2023-07-02 11:05:31 -0700

    Update step_3_quality_check_reads.sh

--- step_3_quality_check_reads.sh
+++ step_3_quality_check_reads.sh
@@ -5,14 +5,14 @@
 #
 #1. look for your data files in the path $dirr/$gene for each gene
 #2. produce quality reports.
-#
+
 
 module load R/4.2.3
 num_graphs=24
 pattern1="_R1.fastq"
 pattern2="_R2.fastq"
 ##parse command line arguments##
-while getopts ":n:g:d:m:p:q:" opt; do
+while getopts ":n:g:d:k:p:q:l:" opt; do
   case $opt in
     n) prefix="$OPTARG"
     ;;
@@ -26,6 +26,8 @@ while getopts ":n:g:d:m:p:q:" opt; do
     ;;
     q) pattern2="$OPTARG"
     ;;
+    l) rlib="$OPTARG"
+    ;;
     \?) echo "Invalid option -$OPTARG" >&2
     exit 1
     ;;
@@ -33,28 +35,19 @@ while getopts ":n:g:d:m:p:q:" opt; do
 done
 
 cd $dirr
+
 ##check how many genes you have, although this should be re-written because the behavior is actually identical. Originally I had it so that you didn't need to keep your files in a containing folder called "gene". But it's much easier to organize this way. This just runs the R script for each gene and produces separate plots.
 head -n1 $gene | sed "s:\t:\n:g" > list_of_genes.txt
-genelist=list_of_genes.txt
-n=$(wc -l $genelist | awk '{print $1}')
-mkdir -p reports
-if [[ $n -eq 1 ]]
-then 
-	gene=$( head -n1 $genelist | awk '{print $1}')
+
+mkdir -p ${dirr}/reports
+
+while read p;
+do
+	gene=${p}
 	mkdir -p ${gene}_dada_out
-	Rscript ${dirr}/scripts/quality_check_reads_in_DADA2.R -d ${dirr} -g ${gene} -p ${pattern1} -q ${pattern2} -n ${prefix} -k ${num_graphs}
+	Rscript ${dirr}/scripts/quality_check_reads_in_DADA2.R ${rlib} -d ${dirr} -g ${gene} -p ${pattern1} -q ${pattern2} -n ${prefix} -k ${num_graphs}
 	mv ${dirr}/${gene}/${prefix}*.pdf ${dirr}/reports/
-else
-	x=1
-	while [[ $x -le $n ]]
-	do
-		ln="sed -n ${x}p $genelist"
-		gene=$($ln)
-		mkdir -p ${gene}_dada_out
-	        Rscript ${dirr}/scripts/quality_check_reads_in_DADA2.R -d ${dirr} -g ${gene} -p ${pattern1} -q ${pattern2} -n ${prefix} -k ${num_graphs}
-        	mv ${dirr}/${gene}/${prefix}*.pdf ${dirr}/reports/
-		x=$(( $x + 1 ))
-	done
-fi
+done < list_of_genes.txt
+
 
 
