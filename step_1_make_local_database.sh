57316573167530861ae01b5cd9e2f4e18bc96e04
sckieran <53580356+sckieran@users.noreply.github.com>
2023-07-05 11:00:07 -0700

    Update step_1_make_local_database.sh

--- step_1_make_local_database.sh
+++ step_1_make_local_database.sh
@@ -37,6 +37,6 @@ mkdir -p $db_dirr
 module load R/4.2.3
 module load ncbi-blast
 #run rentrez#
-Rscript ${dirr}/scripts/query_rentrez.R ${rlib} -n $prefix -t $taxlist -g $genelist -d ${dirr}/$db_dirr -r $retmax 
+Rscript ${dirr}/scripts/query_rentrez.R -z ${rlib} -n $prefix -t $taxlist -g $genelist -d ${dirr}/$db_dirr -r $retmax 
 
 echo "done downloading reference sequences. If you have additional off-target sequences to add here, simply ensure that they are in this folder, that each file name ends *_gene1_sequences.fasta and that each sequence line begins with a '>' and a valid NCBI accession number to continue. You must have a different copy of these fastas for each gene1...geneN that you are building a database for, the program will only include fastas for a gene's reference if it ends *_gene1_sequences.fasta."
