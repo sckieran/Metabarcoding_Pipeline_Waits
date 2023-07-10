92d55259070de439c8aac3f069775d305dbcf7fa
sckieran <53580356+sckieran@users.noreply.github.com>
2023-07-10 12:34:04 -0700

    Update step_4_wrapper.sh

--- step_4_wrapper.sh
+++ step_4_wrapper.sh
@@ -14,6 +14,14 @@ genelist=$PWD/genelist #path to genelist
 params_file="params_file" #name of params_file prefix. All parameter filenames should start with this prefix and end with _gene1,_gene2...geneN for each gene/primer set in the genelist. Default is params_file
 localdat= #only put something here if your local database is not named "yourproject_gene_reference", which is the default for steps 1 and 2.
 rlib="~/Rpackages" #path to your Rpackages
+cutoff=98
+return_low=TRUE
+score="pident"
+local="local"
 
-bash step_4_filter_and_blast_local.sh -d ${dir} -n ${prefix} -g ${genelist} -p ${pattern1} -q ${pattern2} -m ${params_file} -l ${localdat} -b ${rlib}
+
+bash ${dirr}/scripts/step_4_start.sh -d ${dir} -n ${prefix} -g ${genelist} -p ${pattern1} -q ${pattern2} -m ${params_file} -l ${localdat} -b ${rlib} -c ${cutoff} -t ${return_low} -s ${score} -x ${local}
+
+
+
 
