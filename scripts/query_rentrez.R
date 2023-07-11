6f3daf1efe955bd960ee0497542077b6d2818591
sckieran <53580356+sckieran@users.noreply.github.com>
2023-07-11 07:26:39 -0700

    Update query_rentrez.R

--- scripts/query_rentrez.R
+++ scripts/query_rentrez.R
@@ -17,9 +17,12 @@ option_list <- list(make_option(c('-t','--taxfile'), action='store', type='chara
                     make_option(c('-d','--directory'), action='store', type='character', default="~", help='full path to output directory'),
                     make_option(c('-n', '--name'), action='store', type='character', default='your_project', help='prefix for naming outfiles'),
                     make_option(c('-z','--rlib'), action='store', type='character', default="~/Rpackages", help='r library path'),
+                    make_option(c('-k','--key'), action='store', type='numeric', default="416858084f0b7a195302d9a4cff9627e5308", help='your NCBI api key'),
                     make_option(c('-r', '--retmax'), action='store', type='integer', default=10, help='how many matching fastas to return. Recommend <100')
 )
 opt <- parse_args(OptionParser(option_list = option_list))
+set_entrez_key(opt$key)
+Sys.getenv("ENTREZ_KEY")
 
 ##taxa format: taxid and taxname. Can use either one but need to update colnames to reflect data##
 setwd(opt$directory)
@@ -108,70 +111,3 @@ rm(all_tax_avail_wide)
 rm(all_tax_all_genes)
 write.table(all_tax_out, file=paste0(prefix,"_database_taxa_summary.txt"), row.names=FALSE, col.names=TRUE, sep="\t",quote=FALSE,eol="\n")
 }
-
-
-
-
-
-
-###NO HIT EXPLORATIONS. these are pretty project-specific. Use only if you sort of get what's going on.##
-
-
-
-
-##find sequences for congeners for the target species in our database with no seqs in the genus##
-#genus <- filter(all_tax_all_genes, avail_seq=="no")
-#taxlist <- data.frame("taxname"=c(str_split_fixed(genus$taxname," ",2)[,1]), "species"=c(str_split_fixed(genus$taxname," ",2)[,2]))
-#pos_genera_all <- filter(all_tax_all_genes, avail_seq=="yes")
-#pos_genera <- str_split_fixed(pos_genera_all$taxname," ",2)[,1]
-#pos <- unique(taxlist[!(taxlist$taxname %in% pos_genera), ][1])
-#taxlist <- pos
-#for(y in 1:n){
-#gene_terms <- str_c(na.omit(genelist[,y]), collapse=" OR ") ##I picked some common alternate names/spellings for each barcode. So the code loops over each column, which represents a barcode, and then creates a search term that includes all the alternate spellings I could think of.##
-#i=1
-#for (i in 1:z) {
-#  results <- entrez_search(db="nucleotide", term=paste0(taxlist$taxname[i],"[ORGN] AND (",gene_terms,")"),retmax=10) ##search the taxid and the barcode##
-#  #print(results)
-#  if (length(results$ids) > 0) { ##if there are any results##
-#    avail_seq <- "yes"
-#    num_avail <- results$count ##number of matching results##
-#    summs <- entrez_summary(db="nucleotide", id=results$ids[1]) ##summary of first result (it's so slow to grab all, plus it's tough to do anything with that much data)
-#    taxid <- summs[[10]]
-#    accession <- summs[[3]]
-#    temp.fasta <- entrez_fetch(db="nucleotide", id=results$ids, rettype="fasta")
-#    temp.data <- data.frame("taxname" = taxlist$taxname[i], "taxid" = as.character(taxid), "avail_seq"=avail_seq, "num_records"=num_avail,"accession"=accession) ##build individual dataframe.
-#    assign(paste0(taxlist$taxname[i],"_",colnames(genelist[y])),temp.data)
-#    write(temp.fasta,file=paste0(prefix,"_",taxlist$taxname[i],"_genus_",colnames(genelist[y]),"sequences.fasta"))
-#    rm(temp.data)
-#    rm(temp.fasta)
-#    rm(summs)
-#  } else {
-#  avail_seq <- "no"
-#  accession <- "NA"
-#  fasta_seq <- "NA"
-#  taxid <- "NA"
-#  temp.data <- data.frame("taxname" = taxlist$taxname[i], "taxid" = taxid, "avail_seq"=avail_seq, "num_records"=0,"accession"=accession, "fasta"=fasta_seq)
-#  assign(paste0(taxlist$taxname[i],"_",colnames(genelist[y])),temp.data)
-#  rm(temp.data)
-#}
-#}
-##aggregate into a single table and remove all those extra data frames##
-#list_of_gene <-  mget(ls(pattern = paste0("_",colnames(genelist[y]))))
-#temp_gene_all <- bind_rows(list_of_gene)
-#temp_gene_all$gene <- colnames(genelist[y]) ##add a column with barcode ID
-#assign(paste0(colnames(genelist[y]),"_all"),temp_gene_all)
-#rm(list = ls(pattern = paste0("_",colnames(genelist[y]),"$"))) ##clean up data
-#rm(temp_gene_all)
-#}
-
-#list_of_all3 <-  mget(ls(pattern ="_all$"))
-##your whole list: each taxID has a row for each gene (so should be 3x3) that includes the accession # and FASTA for the first hit for each one, and the number of records##
-#genera_wide_all_genes <- bind_rows(list_of_all3)
-
-
-
-
-
-
-
-
