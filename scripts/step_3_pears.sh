8ec179ae98bb91d2e18b4284202a5ab7039f3434
sckieran <53580356+sckieran@users.noreply.github.com>
2023-07-28 15:33:47 -0700

    Create step_3_pears.sh

---
+++ scripts/step_3_pears.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pattern=$1
+r2_pattern=$2
+dir=$3
+
+cd ${dir}
+
+module load pears
+
+for fil in *_${pattern};
+do
+  base=$(echo $fil | awk -F"$pattern" '{print $1}')
+  pear -f ${fil} -r ${base}_${r2_pattern} -o ${base}_paired -j 4
+done
+
+mkdir -p unpaired paired collapsed seqfiles
+mv *_${pattern} ./unpaired/
+mv *_${r2_pattern} ./unpaired/
