308067869a618e327a3f7aeb0ab5d80ef5f7b372
sckieran <53580356+sckieran@users.noreply.github.com>
2023-07-29 16:07:30 -0700

    Update step_8_filter_data.sh

--- scripts/step_8_filter_data.sh
+++ scripts/step_8_filter_data.sh
@@ -7,9 +7,10 @@ gene=$2
 cutoff=$3
 ident=$4
 dir=$5
+rlib=$6
 
 cd ${dir}/${gene}_out
 
-Rscript ${dir}/scripts/filter_id_taxa.R ${prefix}_${gene}_unfiltered_taxatable.txt ${dir}/${gene}_out $prefix $gene $cutoff $ident
+Rscript ${dir}/scripts/filter_id_taxa.R ${prefix}_${gene}_unfiltered_taxatable.txt ${dir}/${gene}_out $prefix $gene $cutoff $ident $rlib
 
 cp ${prefix}_${gene}_filtered_taxatable.txt ${dir}/results_tables/
