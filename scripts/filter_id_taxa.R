10ed57ff9d7dda989805f2f3d6ae53573aab39bc
sckieran <53580356+sckieran@users.noreply.github.com>
2023-07-29 15:51:43 -0700

    Update filter_id_taxa.R

--- scripts/filter_id_taxa.R
+++ scripts/filter_id_taxa.R
@@ -10,7 +10,7 @@ un <- unique(ts$sample)
 for (i in 1:length(un)) {
 t1 <- ts %>% filter(sample == un[i] & identity > args[6])
 test <- ts %>% filter(sample == un[i] & identity > args[6]) %>% group_by(taxa) %>% summarise(sum(reads))
-cutoff <- sum(test$`sum(reads)`) * args[5]
+cutoff <- sum(test$`sum(reads)`) * as.numeric(args[5])
 passing <- test %>% filter(`sum(reads)` > cutoff)
 t1 <- t1[t1$taxa %in% passing$taxa,]
 assign(paste0(i,"_t1"),t1)
