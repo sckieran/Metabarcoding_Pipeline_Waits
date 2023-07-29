13d108cbe8bc7db8eb0833048e148503045f24bc
sckieran <53580356+sckieran@users.noreply.github.com>
2023-07-28 18:06:49 -0700

    Update step_3_pears.sh

--- scripts/step_3_pears.sh
+++ scripts/step_3_pears.sh
@@ -8,10 +8,27 @@ cd ${dir}
 
 module load pear
 
-for fil in *${pattern};
+ls *{pattern} > seqlist
+num_seqs=$( cat seqlist | wc -l | awk '{print $1}')
+tot_per_file=$(( $num_seqs / $max_jobs ))
+
+x=1
+while [[ $x -lt ${max_jobs} ]]
 do
-  base=$(echo $fil | awk -F"$pattern" '{print $1}')
-  pear -f ${fil} -r ${base}${r2_pattern} -o ${base}_paired -j 4
+  if [[ -s diff seqlist ]]
+  then
+    head -n ${tot_per_file} seqlist > seqlist_${x}
+    sed -i "1,${tot_per_file}d" seqlist
+    x=$(( $x + 1 ))
+  else
+  x=$max_jobs
+  fi
+done
+rm seqlist
+
+for fil in seqlist_*;
+do
+  sbatch pear.sh $fil
 done
 
 mkdir -p unpaired paired collapsed seqfiles
