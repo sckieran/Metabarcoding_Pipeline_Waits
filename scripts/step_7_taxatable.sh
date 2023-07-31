ba3da0a8030d719e71fa0e72a8d0e26e3d5bd12a
sckieran <53580356+sckieran@users.noreply.github.com>
2023-07-31 14:14:11 -0700

    Update step_7_taxatable.sh

--- scripts/step_7_taxatable.sh
+++ scripts/step_7_taxatable.sh
@@ -30,31 +30,55 @@ do
   	fi
 done
 rm samplist 	
-
-for fil in samplist_*
-do
-	echo "making taxtable, doing $fil"
-	sbatch ${dirr}/scripts/run_ttb.sh $fil $dirr $gene $prefix
-done
-
 while true;
 do
-        sleep 10s
-        ck="squeue -u ${user}"
-        chck=$($ck)
-        check=$(echo "$chck" | grep "ttb" | wc -l | awk '{print $1}')
-        echo "there are $check jobs left"
-	if [[ $check -eq 0 ]];then
-           echo "done with taxatable" 
-           break
-        fi 
+	for fil in samplist_*
+	do
+		echo "making taxtable, doing $fil"
+  		y=$( echo $ fil | awk -F"_" '{print $2}')
+		if [[ ! -f ${prefix}_${gene}_taxatable.txt_${y} ]] && [[ -s ${prefix}_${gene}_taxatable.txt_${y} ]] ;
+  		then
+  			echo "outfile for $fil does not yet exist or is empty. Doing $fil."
+     			sbatch ${dirr}/scripts/run_ttb.sh $fil $dirr $gene $prefix
+		fi
+	done
+ 	while true;
+	do
+       		sleep 2s
+       	 	ck="squeue -u ${user}"
+        	chck=$($ck)
+       		check=$(echo "$chck" | grep "ttb" | wc -l | awk '{print $1}')
+       		echo "there are $check jobs left"
+		if [[ $check -eq 0 ]];then
+           		echo "done with jobs, checking all ran" 
+           		break
+        	fi 
+	done
+  	ls *_taxatable.txt_* > outs_${gene}
+   	num_outs=$( wc -l outs_${gene} | awk '{print $1}')
+    	ls samplist_* > outs_samps
+     	num_samps=$( wc -l outs_samps | awk '{print $1}')
+      	if [[ $num_outs -eq $num_samps ]]; then
+       		echo "all outfiles created and not empty."
+	 	break
+	fi
 done
+
+
 
 cat ${prefix}_${gene}_taxatable.txt_* > ttb
 cat ${prefix}_${gene}_ttb_header ttb > ${prefix}_${gene}_unfiltered_taxatable.txt
 rm ttb ${prefix}_${gene}_ttb_header ${prefix}_${gene}_taxatable.txt_* samplist_*
 
+num_samp2=$( cut -f1 ${prefix}_${gene}_unfiltered_taxatable.txt | sort | uniq | wc -l | awk '{print $1}')
+if [[ $num_samp2 -eq $num_seqs ]]; then
+	echo "all seqfiles successfully incorporated into taxatable."
+else
+ 	echo "there are $num_seqs seqfiles but only $num_samp2 samples in the taxatable. Some of your jobs may not have run correctly."
+fi
+
 echo "done with unfiltered analysis ${gene}. You can find taxa tables and raw ASV tables in your project directory/reports, and if you set filter to TRUE, the next step will filter your data at your read and identity cutoffs."
+
 rm temp*
 mkdir -p ${dirr}/${gene}_out/sample_seqfiles ${dirr}/results_tables
 mv *_seqs.txt ${dirr}/${gene}_out/sample_seqfiles
