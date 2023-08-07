aff3e97c2283474a1b09e791dbcc4caab69afaf0
sckieran <53580356+sckieran@users.noreply.github.com>
2023-08-07 10:21:53 -0700

    Update step_4_collapse.sh

--- scripts/step_4_collapse.sh
+++ scripts/step_4_collapse.sh
@@ -16,6 +16,8 @@ mv *${pattern} ./unpaired/
 mv *${r2_pattern} ./unpaired/
 mv *.discarded.fastq ./paired/discarded/
 mv *.unassembled*.fastq ./paired/unassembled/
+rm seqlist_* 
+rm outslist
 
 #make list of files to collapse#
 ls *_paired.assembled.fastq > pairedlist
@@ -45,13 +47,25 @@ while [[ $num_seqs -ne $num_outs ]];
 do
   for fil in pairedlist_*;
   do
-    echo "doing $fil"
-    sbatch ${dir}/scripts/run_collapser.sh $fil ${dir}/${gene}
-done
-
-while true;
-do
-        sleep 5s
+    while read p;
+    do
+      base=$( echo $p | awk -F"_paired.assembled.fastq" '{print $1}')
+      if [[ ! -f ${base}_collapsed.fasta ]];
+      then
+        echo "$p" >> temp_$fil
+      fi
+    done < ${fil}
+    mv temp_${fil} ${fil}
+    if [[ ! -s "$fil" ]];
+    then
+      sbatch ${dir}/scripts/run_collapser.sh $fil ${dir}/${gene}
+    else
+      echo "all samples for $fil completed already."
+    fi
+  done
+  while true;
+  do
+        sleep 3s
         ck="squeue -u ${user}"
         chck=$($ck)
         check=$(echo "$chck" | grep "fx_col" | wc -l | awk '{print $1}')
@@ -59,7 +73,13 @@ do
            echo "done with collapsing ASVs" 
            break
         fi 
+  done
+  ls *_collapsed.fasta > outslist
+  num_outs=$( wc -l outslist | awk '{print $1}')
 done
 
+echo "there are $num_seqs input samples and $num_outs output samples. Moving on."
+
+rm outslist
 rm pairedlist_*
 mv *_paired.assembled.fastq ./paired/
