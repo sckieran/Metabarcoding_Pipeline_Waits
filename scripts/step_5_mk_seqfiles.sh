2567afa9613c98a6a7b92ae111522d4ca75ce2dc
sckieran <53580356+sckieran@users.noreply.github.com>
2023-08-08 11:09:50 -0700

    Update step_5_mk_seqfiles.sh

--- scripts/step_5_mk_seqfiles.sh
+++ scripts/step_5_mk_seqfiles.sh
@@ -82,6 +82,7 @@ do
   done
   ls *_filtered_seqs.txt > outslist
   num_outs=$( wc -l outslist | awk '{print $1}')
+  echo "there are $num_seqs sequences to make seqfiles for and $num_outs seqfiles. If these numbers don't match, will resubmit jobs as necessary. If these numbers do match, moving on to building a taxonomy table."
 done
 
 rm mksq.*.err
