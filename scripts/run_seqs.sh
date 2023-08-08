39ace029b6d99e16f70a7d06dc2b5c0e8b94c315
sckieran <53580356+sckieran@users.noreply.github.com>
2023-08-08 10:36:39 -0700

    Update run_seqs.sh

--- scripts/run_seqs.sh
+++ scripts/run_seqs.sh
@@ -9,6 +9,7 @@ dir=$2
 gene=$3
 rlib=$4
 cutoff=$5
+minlen=$6
 
 cd ${dir}/${gene}
 module load R/4.2.3
@@ -17,7 +18,7 @@ while read fil;
 do
   base=$(echo $fil | awk -F"_clustered.fasta" '{print $1}')
   grep ">" $fil | awk -F"-" '{print $2}' > temp_reads_${base}
-  grep -v ">" $fil > temp_seqs_${base}
+  grep -v ">" $fil | awk -v m=$minlen '{ if (length($1) > m) print }' > temp_seqs_${base}
   paste  temp_seqs_${base} temp_reads_${base}  > ${base}_seqs.txt
   Rscript ${dir}/scripts/filter_rra.R $rlib ${dir}/${gene} ${base}_seqs.txt $base $cutoff
   rm temp_reads_${base} temp_seqs_${base}
