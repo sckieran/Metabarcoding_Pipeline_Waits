3678c1ea7291ddfdcab984abe2c168ab8c096633
sckieran <53580356+sckieran@users.noreply.github.com>
2023-08-25 14:46:58 -0700

    Update filter_rra.R

--- scripts/filter_rra.R
+++ scripts/filter_rra.R
@@ -1,12 +1,11 @@
 
 args = commandArgs(trailingOnly=TRUE)
-library(lubridate, lib=args[1])
-library(tidyverse,lib=args[1])
-setwd(args[2])
-full <- read.delim(args[3])
-cutoff=as.numeric(args[5])
+library(tidyverse)
+setwd(args[1])
+full <- read.delim(args[2])
+cutoff=as.numeric(args[4])
 colnames(full) <-c("seq","reads")
 filt <- full %>% filter(reads >= (sum(reads)*cutoff) | reads >= 500)
-outname=paste0(args[4],"_filtered_seqs.txt")
+outname=paste0(args[3],"_filtered_seqs.txt")
 write_delim(filt, file=outname, quote="none", col_names=FALSE, delim = "\t")
 
