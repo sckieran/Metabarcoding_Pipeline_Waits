2fec4de27ff3600426aeb89449bf0cfcbf46251a
sckieran <53580356+sckieran@users.noreply.github.com>
2023-08-25 16:25:55 -0700

    Update pipeline_wrapper.sh

--- pipeline_wrapper.sh
+++ pipeline_wrapper.sh
@@ -24,6 +24,7 @@ taxa_rra=0.005
 identity_cutoff=97
 minlen=70
 remote=FALSE
+remote_compare=FALSE
 return_low=TRUE
 user=your_slurm_username
 email=your_ncbi_email_address #technically optional, but NCBI throws warnings if you don't include
@@ -46,7 +47,7 @@ echo "###"
 echo "moving on to step 2 - making your blast database from the downloaded NCBI sequences"
 echo "###"
 
-bash ${dir}/scripts/step_2_make_database.sh -n ${prefix} -h ${db_dirr} -g ${genelist} -d ${dir} -e ${extra_seqs}
+bash ${dir}/scripts/step_2_make_database.sh -n ${prefix} -h ${db_dirr} -g ${genelist} -d ${dir} -e ${extra_seqs} -f ${env_name}
 exit_status=$?
 if [ "${exit_status}" -ne 0 ];
 then
@@ -112,7 +113,37 @@ do
     exit_status=$?
     if [ "${exit_status}" -ne 0 ];
     then
-      echo "Step 5 failed. exit ${exit_status} and restart at step 5."
+      echo "Step 6 failed. exit ${exit_status} and restart at step 6."
+      exit 1
+    fi
+    echo "###"
+    echo "###"
+    echo "done with BLAST. Making raw (unfiltered) taxatable"
+    echo "###"
+    bash ${dir}/scripts/step_7_taxatable.sh ${prefix} ${gene} ${dir} ${max_jobs} ${user}
+    if [ "${exit_status}" -ne 0 ];
+    then
+      echo "Step 7 failed. exit ${exit_status} and restart at step 7."
+      exit 1
+    fi
+elif [[ $remote_comp == "TRUE" ]]
+    then
+    echo "performing both local and remote BLAST and comparing the two taxatables."
+    bash ${dir}/scripts/step_6_blast_comparison.sh -n ${prefix} -g ${gene} -d ${dir} -m ${minlen} -r ${db_dirr} -c ${identity_cutoff} -t ${return_low} -j ${max_jobs} -u ${user}
+    exit_status=$?
+    if [ "${exit_status}" -ne 0 ];
+    then
+        echo "Step 6 failed. exit ${exit_status} and restart at step 6."
+        exit 1
+    fi
+    echo "###"
+    echo "###"
+    echo "done with BLAST. Making raw (unfiltered) taxatable"
+    echo "###"
+    bash ${dir}/scripts/step_7_taxatable_comp.sh ${prefix} ${gene} ${dir} ${max_jobs} ${user}
+    if [ "${exit_status}" -ne 0 ];
+    then
+      echo "Step 7 failed. exit ${exit_status} and restart at step 7."
       exit 1
     fi
   else
@@ -121,21 +152,30 @@ do
     exit_status=$?
     if [ "${exit_status}" -ne 0 ];
     then
-      echo "Step 5 failed. exit ${exit_status} and restart at step 5."
+      echo "Step 6 failed. exit ${exit_status} and restart at step 6."
       exit 1
     fi
-  fi
    echo "###"
    echo "###"
    echo "done with BLAST. Making raw (unfiltered) taxatable"
    echo "###"
    bash ${dir}/scripts/step_7_taxatable.sh ${prefix} ${gene} ${dir} ${max_jobs} ${user}
+    if [ "${exit_status}" -ne 0 ];
+    then
+      echo "Step 7 failed. exit ${exit_status} and restart at step 7."
+      exit 1
+    fi  
+  fi
+
    
-   echo "###"
-   echo "###"
    if [[ $filter == "TRUE" ]]
    then
      echo "Done making raw taxatable. You chose to filter your data by relative read abundance and percent identity. Now filtering per-sample taxa at your relative read abundance cutoff of ${taxa_rra} and your identity cutoff of ${identity_cutoff}."
+     if [[ $remote_comp == "TRUE" ]]
+     then
+         echo "This option cannot be used with remote_compare=TRUE. Skipping this step."
+         exit 1
+     fi
      bash ${dir}/scripts/step_8_filter_data.sh ${prefix} ${gene} ${taxa_rra} ${identity_cutoff} ${dir} ${env_name}
      exit_status=$?
      if [ "${exit_status}" -ne 0 ];
