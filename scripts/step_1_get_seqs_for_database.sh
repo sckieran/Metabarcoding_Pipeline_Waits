d3463deb7910f59e35e4331cffdb7ee3b3a25c44
sckieran <53580356+sckieran@users.noreply.github.com>
2023-08-25 11:32:18 -0700

    Update step_1_get_seqs_for_database.sh

--- scripts/step_1_get_seqs_for_database.sh
+++ scripts/step_1_get_seqs_for_database.sh
@@ -2,7 +2,7 @@
 #
 
 
-while getopts ":n:t:g:d:r:h:l:k:" opt; do
+while getopts ":n:t:g:d:r:h:l:k:p:e:" opt; do
   case $opt in
     n) prefix="$OPTARG"
     ;;
@@ -20,11 +20,16 @@ while getopts ":n:t:g:d:r:h:l:k:" opt; do
     ;;
     k) key="$OPTARG"
     ;;
+    p) env_name="$OPTARG"
+    ;;
+    e) email="$OPTARG"
+    ;;
     \?) echo "Invalid option -$OPTARG" >&2
     exit 1
     ;;
   esac
 done
+cd $dirr
 #make the output directory, if it doesn't exist#
 if [[ -z ${retmax} ]]
 then
@@ -35,10 +40,13 @@ then
 db_dirr=reference_database
 fi
 mkdir -p $db_dirr
+cp $taxlist ./${db_dirr}
+cp $genelist ./${db_dirr}
 
-module load R/4.2.3
-module load ncbi-blast
+#module load R/4.2.3
+#module load ncbi-blast
 #run rentrez#
-Rscript ${dirr}/scripts/query_rentrez.R -z ${rlib} -n $prefix -t $taxlist -g $genelist -d ${dirr}/$db_dirr -r $retmax -k $key
+conda activate $env_name
+python ${dirr}/scripts/query_rentrez.py $prefix ${PWD}/${db_dirr} $genelist $taxlist $retmax $key $email
 
 echo "done downloading reference sequences. If you have additional off-target sequences to add here, simply ensure that they are in this folder, that each file name ends *_gene1_sequences.fasta and that each sequence line begins with a '>' and a valid NCBI accession number to continue. You must have a different copy of these fastas for each gene1...geneN that you are building a database for, the program will only include fastas for a gene's reference if it ends *_gene1_sequences.fasta."
