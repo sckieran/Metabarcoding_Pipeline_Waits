c2167ef04b03e80781bd1d003d68f2521c4e95a1
sckieran <53580356+sckieran@users.noreply.github.com>
2023-08-25 14:10:07 -0700

    Update step_1_get_seqs_for_database.sh

--- scripts/step_1_get_seqs_for_database.sh
+++ scripts/step_1_get_seqs_for_database.sh
@@ -51,4 +51,10 @@ cp $genelist ./${db_dirr}
 conda activate $env_name
 python ${dirr}/scripts/query_rentrez.py $prefix ${PWD}/${db_dirr} $genelist $taxlist $retmax $genus_search $key $email
 
+exit_status=$?
+if [ "${exit_status}" -ne 0 ];
+then
+    echo "exit ${exit_status}"
+    exit 1
+fi
 echo "done downloading reference sequences. If you have additional off-target sequences to add here, simply ensure that they are in this folder, that each file name ends *_gene1_sequences.fasta and that each sequence line begins with a '>' and a valid NCBI accession number to continue. You must have a different copy of these fastas for each gene1...geneN that you are building a database for, the program will only include fastas for a gene's reference if it ends *_gene1_sequences.fasta."
