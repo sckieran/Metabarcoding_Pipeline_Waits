e652613205d06fc0e7f2563761f54633ee6c9cef
sckieran <53580356+sckieran@users.noreply.github.com>
2023-08-25 14:33:47 -0700

    Update step_2_make_database.sh

--- scripts/step_2_make_database.sh
+++ scripts/step_2_make_database.sh
@@ -29,22 +29,6 @@ cp $genelist $db_dirr
 cp ${extra}*.fasta $db_dirr
 cd $db_dirr
 
-for fil in *_sequences.fasta
-do
-	grep ">" $fil | awk -F" " '{print $1}' > temp_lines
-	echo "adding taxid to $fil"
-	sed -i 's/>//g' temp_lines
-	while read p; #for each sequence in each fasta, query NCBI's taxid lookup to get taxid, and attach it to the end of the sequence title.
-	do
- 		taxid=$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=${p}&rettype=fasta&retmode=xml" | grep TSeq_taxid | cut -d '>' -f 2 | cut -d '<' -f 1 | tr -d "\n" | awk '{print $1}')
-  		if [[ ! -z $taxid ]]
-    		then
-      			sed -i "/${p}/ s/$/\ttaxid=${taxid}/" $fil
-		else
-  			echo "no taxid for accession $p"
-     		fi
-	done < temp_lines
-done
 rm temp_lines
 echo "done processing fastas, building database."
 #loop over each gene in your genelist#
@@ -54,7 +38,10 @@ do
         mkdir -p "$p"
         cat *_${p}_sequences.fasta > ${prefix}_${p}_sequence_database.fasta
  	echo "checking for duplicate sequences in database fasta"
-	bash ${dirr}/scripts/step_2_p1_rmdups.sh  ${prefix} ${p}     
+	eval "$(conda shell.bash hook)"
+	conda activate $env_name
+ 	python ${dirr}/scripts/step_2_p1_rmdups.py  ${prefix}_${p}_sequence_database.fasta $PWD    
+ 	mv temp_out ${prefix}_${p}_sequence_database.fasta
 	makeblastdb -dbtype nucl -in ${prefix}_${p}_sequence_database.fasta -out ${prefix}_${p}_reference  -parse_seqids -blastdb_version 5
         mv *_${p}_sequences.fasta ./${p}
 done < list_of_genes.txt
