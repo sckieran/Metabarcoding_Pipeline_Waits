9a8bd043c8887b5192fb05751b961ad6527ee8ae
sckieran <53580356+sckieran@users.noreply.github.com>
2023-08-31 11:26:56 -0700

    Update step_8_filter_data.sh

--- scripts/step_8_filter_data.sh
+++ scripts/step_8_filter_data.sh
@@ -12,6 +12,6 @@ env_name=$6
 cd ${dir}/${gene}_out
 eval "$(conda shell.bash hook)"
 conda activate $env_name
-Rscript ${dir}/scripts/filter_id_taxa.R ${prefix}_${gene}_unfiltered_taxatable.txt ${dir}/${gene}_out $prefix $gene $cutoff $ident
+Rscript ${dir}/scripts/filter_id_taxa.R ${prefix}_${gene}_unfiltered_taxatable.txt ${dir}/${gene}_out $prefix $gene $cutoff $ident ${CONDA_PREFIX}/lib/R/library/
 
 cp ${prefix}_${gene}_filtered_taxatable.txt ${dir}/results_tables/
