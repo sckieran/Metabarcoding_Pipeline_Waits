0727aa5c4838e84056965d161c104c0585ab0660
sckieran <53580356+sckieran@users.noreply.github.com>
2023-09-01 09:36:40 -0700

    Update run_seqs.sh

--- scripts/run_seqs.sh
+++ scripts/run_seqs.sh
@@ -19,14 +19,20 @@ pref=$CONDA_PREFIX
 
 while read fil;
 do
-  base=$(echo $fil | awk -F"_clustered.fasta" '{print $1}')
-  grep ">" $fil | awk -F"-" '{print $2}' > temp_reads_${base}
-  grep -v ">" $fil  > temp_seqs_${base}
-  paste  temp_seqs_${base} temp_reads_${base}  > ${base}_seqs.txt
-  cat ${base}_seqs.txt | awk -v m=$minlen '{ if (length($1) > m) print }' > temp_${base}_seqs.txt
-  mv temp_${base}_seqs.txt ${base}_seqs.txt
-  Rscript ${dir}/scripts/filter_rra.R ${dir}/${gene} ${base}_seqs.txt $base $cutoff ${pref}/lib/R/library/
-  mv ${base}_seqs.txt ./unfiltered_seqfiles/${base}_seqs.txt
-  rm temp_reads_${base} temp_seqs_${base}
+  if [[ -s $fil ]];
+  then
+    base=$(echo $fil | awk -F"_clustered.fasta" '{print $1}')
+    grep ">" $fil | awk -F"-" '{print $2}' > temp_reads_${base}
+    grep -v ">" $fil  > temp_seqs_${base}
+    paste  temp_seqs_${base} temp_reads_${base}  > ${base}_seqs.txt
+    cat ${base}_seqs.txt | awk -v m=$minlen '{ if (length($1) > m) print }' > temp_${base}_seqs.txt
+    mv temp_${base}_seqs.txt ${base}_seqs.txt
+    Rscript ${dir}/scripts/filter_rra.R ${dir}/${gene} ${base}_seqs.txt $base $cutoff ${pref}/lib/R/library/
+    mv ${base}_seqs.txt ./unfiltered_seqfiles/${base}_seqs.txt
+    rm temp_reads_${base} temp_seqs_${base}
+  else
+    echo "$fil is empty or does not exist. Omitting $fil to avoid downstream errors. Check infiles and error logs for step 3 and 4 for $fil."
+    echo -n "" > ${base}_filtered_seqs.txt
+    echo -n "" > ${base}_seqs.txt
 done < ${inp}
 
